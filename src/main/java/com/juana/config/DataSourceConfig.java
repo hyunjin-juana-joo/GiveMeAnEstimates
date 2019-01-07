4660f1dc7ea3bb58dcc71fb2c1cef9f6428bfa46
juana.joo <juana.joo@within.co.kr>
2019-01-07 21:15:37 +0900

    replica 제거 (싱글디비)

--- src/main/java/com/juana/config/DataSourceConfig.java
+++ src/main/java/com/juana/config/DataSourceConfig.java
@@ -1,52 +1,21 @@
 package com.juana.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Configuration
 public class DataSourceConfig {
-
-    @Bean
-    @ConfigurationProperties(prefix="database.datasource-read")
-    public DataSource readDataSource(){
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix="database.datasource-write")
-    public DataSource writeDataSource(){
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    public DataSource routingDataSource(@Qualifier("writeDataSource") DataSource writeDataSource,
-                                        @Qualifier("readDataSource") DataSource readDataSource){
-
-        ReplicaRoutingDataSourceConfig routingDataSource = new ReplicaRoutingDataSourceConfig();
-        Map<Object, Object> dataSourceMap = new HashMap<>();
-        dataSourceMap.put("write", writeDataSource);
-        dataSourceMap.put("read", readDataSource);
-        routingDataSource.setTargetDataSources(dataSourceMap);
-        routingDataSource.setDefaultTargetDataSource(readDataSource);
-
-        return readDataSource;
-    }
-
+    @Bean(name = "dataSource")
     @Primary
-    @Bean
-    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource){
-        return new LazyConnectionDataSourceProxy(routingDataSource);
+    @ConfigurationProperties(prefix="spring.jpa.database.datasource")
+    public DataSource defaultDataSource(){
+        return DataSourceBuilder.create().build();
     }
-
 }
